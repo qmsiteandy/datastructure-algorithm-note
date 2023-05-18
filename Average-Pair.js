e53c1294ceb997151b5aa55b835a2649748b29e3
ht93084 <ht93084@gmail.com>
2023-05-18 21:36:47 +0800

    Add: answer 2 of Average-Pair

--- Average-Pair.js
+++ Average-Pair.js
@@ -1,16 +1,40 @@
+// 我的方法：O(n)
+// function averagePair(arr, avg) {
+//   let result = [];
+//   let obj = {}; // 用 obj 紀錄有哪些項目，因為 object access：O(1)
+
+//   for (let i of arr) {
+//     obj[i] = true;
+//   }
+
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let need = avg * 2 - arr[i]; // 需要pair的value
+//     // 如果有對應的項目
+//     if (obj[need]) {
+//       result.push([arr[i], need]);
+//     }
+//   }
+
+//   return result;
+// }
+
+// 範例解法
 function averagePair(arr, avg) {
+  let left = 0;
+  let right = arr.length - 1;
   let result = [];
-  let obj = {}; // 用 obj 紀錄有哪些項目，因為 object access：O(1)
 
-  for (let i of arr) {
-    obj[i] = true;
-  }
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    let need = avg * 2 - arr[i]; // 需要pair的value
-    // 如果有對應的項目
-    if (obj[need]) {
-      result.push([arr[i], need]);
+  while (left <= right) {
+    let total = arr[left] + arr[right];
+    // 相加太小，left+1
+    if (total < avg * 2) left++;
+    // 相加太大，right-1
+    else if (total > avg * 2) right--;
+    // 剛好符合，紀錄，並且 left+1、right-1
+    else {
+      result.push([left, right]);
+      left++;
+      right++;
     }
   }
 
